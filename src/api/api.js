e628038766f1c288fc990b34b0fe1d1558533637
Manikandan <manikandan.work.it@gmail.com>
2025-09-02 14:15:46 +0530

    api connect

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,23 @@
+// import axios from "axios";
+
+// const API = axios.create({
+//   baseURL: "http://localhost:8000/api",  // backend URL
+// });
+
+// // Attach token automatically
+// API.interceptors.request.use((config) => {
+//   const token = localStorage.getItem("token");
+//   if (token) config.headers.Authorization = `Bearer ${token}`;
+//   return config;
+// });
+
+// export default API;
+
+
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8000/api",  // backend URL
+  baseURL: "https://webillz-backend.onrender.com/api",  // ✅ deployed backend URL
 });
 
 // Attach token automatically
